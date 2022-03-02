a89440f8104cfd3be6b825af05e71c0d96c0dc7c
Salvador Rubio <salvarubiomartinez@gmail.com>
2022-03-02 18:34:39 +0100

    tags to icons

--- config.def.h
+++ config.def.h
@@ -32,7 +32,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "🌐", "", "", "", "", "", "", "🗁", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
